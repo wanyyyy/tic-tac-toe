d3d7938ff7854976c7a75a4c82c5179e5cd452e9
KimDaewan <wan1471@gmail.com>
2024-03-26 14:35:40 +0900

    스코어보드 추가

--- src/app/layout/board/tic-tac-toe.tsx
+++ src/app/layout/board/tic-tac-toe.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
+import resetIcons from "../../../../public/icons/reset.png";
+
 import Square from "../../components/square/square";
-import styles from "./board.module.css";
 import Header from "@/app/components/header/header";
+import styles from "./board.module.css";
 
 export default function Board() {
   const [squareArray, setSquareArray] = useState(Array(9).fill(null));
   const [isNext, setIsNext] = useState(true);
   const [winner, setWinner] = useState(false);
   const [winnerText, setWinnerText] = useState("");
+  const [homeWinner, setHomeWinnder] = useState(0);
+  const [awayWinner, setAwayWinnder] = useState(0);
 
   const handleWinner = (squareArray: (string | null)[]) => {
     const winnerCase = [
@@ -32,6 +37,9 @@ export default function Board() {
       ) {
         data[0] !== null ? setWinnerText(data[0]) : null;
         setWinner(true);
+        data[0] === "⚫️"
+          ? setHomeWinnder(homeWinner + 1)
+          : setAwayWinnder(awayWinner + 1);
       }
     });
   };
@@ -51,6 +59,7 @@ export default function Board() {
   const handleReset = () => {
     setSquareArray(Array(9).fill(null));
     setWinner(false);
+    setIsNext(true);
     setWinnerText("");
   };
 
@@ -58,26 +67,67 @@ export default function Board() {
     <div>
       <Header>Tic-Tac-Toe</Header>
       <div className={styles.reset}>
-        <button onClick={handleReset}>리겜</button>
+        <button onClick={handleReset}>다시하기</button>
       </div>
-      <div className={styles.board}>
-        {squareArray.map((_, squareNumber: number) => {
-          return (
-            <Square
-              onClick={() => handleClick(squareNumber)}
-              disabled={squareArray[squareNumber]}
-              key={squareNumber}
-            >
-              {squareArray[squareNumber]}
-            </Square>
-          );
-        })}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          alignItems: "center",
+          height: "70px",
+        }}
+      >
+        <div
+          style={{
+            gridColumn: "2/3",
+            textAlign: "center",
+            fontSize: "30px",
+          }}
+        >
+          ⚫️ {homeWinner} : {awayWinner} ⚪️
+        </div>
+        <button
+          style={{
+            backgroundColor: "#fff",
+            border: "0",
+            borderRadius: "8px",
+            height: "30px",
+            width: "30px",
+            cursor: "pointer",
+          }}
+          onClick={() => (setHomeWinnder(0), setAwayWinnder(0))}
+        >
+          <Image src={resetIcons} alt="점수리셋" width={20} height={20} />
+        </button>
       </div>
-      {!winner && squareArray.indexOf(null) === -1 ? (
-        <div className={styles.result}>비김!</div>
-      ) : winnerText ? (
-        <div className={styles.result}>{winnerText} 승리!</div>
-      ) : null}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className={styles.board}>
+          {squareArray.map((_, squareNumber: number) => {
+            return (
+              <Square
+                onClick={() => handleClick(squareNumber)}
+                disabled={squareArray[squareNumber]}
+                key={squareNumber}
+              >
+                {squareArray[squareNumber]}
+              </Square>
+            );
+          })}
+        </div>
+      </div>
+      <div className={styles.result}>
+        {!winner && squareArray.indexOf(null) === -1
+          ? "비김!"
+          : winnerText
+          ? `${winnerText} 승리!`
+          : null}
+      </div>
     </div>
   );
 }
